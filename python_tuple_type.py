acdc9c6e44ccc99c3b9fce97b7da14a72391186d
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-04-03 01:50:33 +0800

    initial commit

---
+++ python_tuple_type.py
@@ -0,0 +1,28 @@
+input = str(input("enter a list: "))
+input = input.strip('][')
+a = input.split(', ')
+
+for i in range(len(a)):
+    print(a[i])
+    try:
+        x = complex(a[i])
+        if x.imag != 0:
+            a[i] = x
+        elif x.imag == 0:
+            try:
+                x = float(a[i])
+                print(x.is_integer())
+                if x.is_integer():
+                    x = int(a[i])
+                    a[i] = x
+                else:
+                    a[i] = x
+                    
+            except:
+                continue
+    except:
+        x = str(a[i])
+        x = x.strip("'")
+        a[i] = x
+
+print(a)
